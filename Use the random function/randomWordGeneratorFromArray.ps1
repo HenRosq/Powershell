ffdf455d3a0cc35ccedd9bca431d07c7812ed8e1
Henrik Rosqvist <112177702+HenRosq@users.noreply.github.com>
2022-10-12 15:00:43 +0200

    Add files via upload

---
+++ Use the random function/randomWordGeneratorFromArray.ps1
@@ -0,0 +1,19 @@
+﻿ # Word list
+ $words = @('rabbit', 'peccary', 'colt', 'anteater',
+     'meerkat', 'eagle', 'owl', 'cow', 'turtle', 'bull',
+     'baselisk', 'snake', 'lizzard', 'panda', 'bear', 'pig',
+     'lion', 'tiger', 'bunny', 'wolf', 'deer', 'pronghorn',
+     'fish', 'rabbit', 'gorilla', 'puma', 'mustang', 'sheep',
+     'wolverine', 'hyena', 'beaver', 'rooster', 'ox', 'frog'
+ )
+ 
+ Function Create-Wordsss 
+ {
+     $word1 = Get-Random $words # get first random word from word list
+     Write-Host "First word " $word1 -ForegroundColor Green # write output word1
+
+     return $word1
+ }
+
+
+ Create-Wordsss
