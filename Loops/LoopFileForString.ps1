61b549cb51b127de41afeb7d691829c0f86159b2
Henrik Rosqvist <112177702+HenRosq@users.noreply.github.com>
2022-10-12 11:52:18 +0200

    Add files via upload

---
+++ Loops/LoopFileForString.ps1
@@ -0,0 +1,10 @@
+﻿$text = Get-Content H:\testa.txt
+$pattern = "*Henrik*"
+
+for ($i=1;  $i -lt $text.Length; $i++)
+{
+    if($text[$i] -like $pattern)
+    {
+        "Line $i contains `"$pattern`""
+    }
+}
